c2d32293674472a2c1f4eb1ddcf2d6d1855abb0e
Martin Hurton <hurtonm@gmail.com>
2014-03-22 14:42:53 +0100

    Add zmtp_dealer class

--- include/zmtp.h
+++ include/zmtp.h
@@ -8,8 +8,29 @@ extern "C" {
 #include <assert.h>
 #include <stddef.h>
 #include <string.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 typedef unsigned char byte;
+
+enum zmtp_socket_type {
+    ZMTP_PAIR = 0,
+    ZMTP_PUB,
+    ZMTP_SUB,
+    ZMTP_REQ,
+    ZMTP_REP,
+    ZMTP_DEALER,
+    ZMTP_ROUTER,
+    ZMTP_PULL,
+    ZMTP_PUSH,
+    ZMTP_XPUB,
+    ZMTP_XSUB,
+    ZMTP_STREAM
+
+};
 
 #ifdef __cplusplus
 }
---
+++ src/zmtp_dealer.c
@@ -0,0 +1,84 @@
+//  Implementation of DEALER socket
+
+#include "../include/zmtp_connection.h"
+#include "../include/zmtp_dealer.h"
+#include "../include/zmtp.h"
+
+struct zmtp_dealer {
+    zmtp_connection_t *conn;
+};
+
+zmtp_dealer_t *
+zmtp_dealer_new (int fd)
+{
+    zmtp_dealer_t *self = (zmtp_dealer_t *) malloc (sizeof *self);
+    if (self) {
+        *self = (zmtp_dealer_t) { .conn = zmtp_connection_new (fd, ZMTP_DEALER) };
+        if (!self->conn) {
+            free (self);
+            return NULL;
+        }
+        if (zmtp_connection_negotiate (self->conn) == -1) {
+            zmtp_connection_destroy (&self->conn);
+            free (self);
+            return NULL;
+        }
+    }
+    return self;
+}
+
+zmtp_dealer_t *
+zmtp_dealer_ipc_connect (const char *addr)
+{
+    //  Create socket
+    const int s = socket (AF_UNIX, SOCK_STREAM, 0);
+    if (s == -1)
+        return NULL;
+    struct sockaddr_un remote =
+        (struct sockaddr_un) { .sun_family = AF_UNIX };
+    strcpy (remote.sun_path, addr);
+    const socklen_t len = strlen (remote.sun_path) + sizeof remote.sun_family;
+    //  Connect the socket
+    const int rc = connect (s, (const struct sockaddr *) &remote, len);
+    if (rc == -1) {
+        close (s);
+        return NULL;
+    }
+    else {
+        zmtp_dealer_t *self = zmtp_dealer_new (s);
+        if (!self)
+            close (s);
+        return self;
+    }
+}
+
+void
+zmtp_dealer_destroy (zmtp_dealer_t **self_p)
+{
+    assert (self_p);
+
+    if (*self_p) {
+        zmtp_dealer_t *self = *self_p;
+        zmtp_connection_destroy (&self->conn);
+        free (self);
+        *self_p = NULL;
+    }
+}
+
+int
+zmtp_dealer_send (zmtp_dealer_t *self, zmtp_msg_t *msg)
+{
+    assert (self);
+    assert (self->conn);
+
+    return zmtp_connection_send (self->conn, msg);
+}
+
+zmtp_msg_t *
+zmtp_dealer_recv (zmtp_dealer_t *self)
+{
+    assert (self);
+    assert (self->conn);
+
+    return zmtp_connection_recv (self->conn);
+}
